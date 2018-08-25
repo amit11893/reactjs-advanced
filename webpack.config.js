f2c8cc6f31606b47d5557981bab4d7e11dec3b65
Amit Kumar <amit11893@gmail.com>
2018-08-25 17:45:14 +0530

    add react and configure webpack to produce bundle.js

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry: ['babel-polyfill', './lib/components/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    //mode: 'development',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        }]
+    }
+};
